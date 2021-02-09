69f382f8855956ff0f2b50e8ba4564054a6782fb
aleksandarskrbic <skrbic.alexa@gmail.com>
2021-02-09 15:14:23 +0100

    Reformat code

--- modules/processor/src/main/scala/io/scalac/ms/processor/service/Enrichment.scala
+++ modules/processor/src/main/scala/io/scalac/ms/processor/service/Enrichment.scala
@@ -21,23 +21,26 @@ object Enrichment {
     ZIO.accessM(_.get.enrich(transactionRaw))
 
   lazy val live: ZLayer[
-    Logging with CountryCache with SttpClient, Nothing, Enrichment
+    Logging with CountryCache with SttpClient,
+    Nothing,
+    Enrichment
   ] = ZLayer.fromFunction { env =>
-      new Service {
-        override def enrich(
-                             transactionRaw: TransactionRaw
-                           ): IO[ProcessorError, TransactionEnriched] =
-          (for {
-            _       <- log.info(s"Getting country details from cache for ${transactionRaw.country}.")
-            country <- CountryCache.get(transactionRaw.country)
-            result  <- country.fold(
-              fetchAndCacheCountryDetails(transactionRaw.country)
-            )(ZIO.succeed(_))
-          } yield toTransactionEnriched(
-            transactionRaw, result
-          )).provide(env)
-      }
+    new Service {
+      override def enrich(
+        transactionRaw: TransactionRaw
+      ): IO[ProcessorError, TransactionEnriched] =
+        (for {
+          _       <- log.info(s"Getting country details from cache for ${transactionRaw.country}.")
+          country <- CountryCache.get(transactionRaw.country)
+          result <- country.fold(
+                     fetchAndCacheCountryDetails(transactionRaw.country)
+                   )(ZIO.succeed(_))
+        } yield toTransactionEnriched(
+          transactionRaw,
+          result
+        )).provide(env)
     }
+  }
 
   private def fetchAndCacheCountryDetails(
     countryName: String
@@ -49,17 +52,17 @@ object Enrichment {
     } yield country
 
   private def fetchCountryDetails(
-                                   countryName: String
-                                 ): ZIO[SttpClient, ProcessorError, Country] =
+    countryName: String
+  ): ZIO[SttpClient, ProcessorError, Country] =
     for {
-      req     <- ZIO.succeed(
-        basicRequest.get(urlOf(countryName)).response(asJson[List[Country]])
-      )
-      res     <- SttpClient.send(req).orElseFail(CountryApiUnreachable)
+      req <- ZIO.succeed(
+              basicRequest.get(urlOf(countryName)).response(asJson[List[Country]])
+            )
+      res <- SttpClient.send(req).orElseFail(CountryApiUnreachable)
       country <- res.body.fold(
-        _ => ZIO.fail(ResponseExtractionError),
-        res => ZIO.succeed(res.head)
-      )
+                  _ => ZIO.fail(ResponseExtractionError),
+                  res => ZIO.succeed(res.head)
+                )
     } yield country
 
   private def urlOf(countryName: String): Uri =
